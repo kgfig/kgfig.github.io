b8557d63406fb16b9d4ad1cd9fb6c6540519c2a7
KG Figueroa <kathleengay.figueroa@gmail.com>
2021-04-12 23:28:21 +0800

    Barebones toggleable navbar

---
+++ js/mobile-nav.js
@@ -0,0 +1,21 @@
+const toggleCollapsibleNav = function () {
+  toggleVisibility("nav-collapse");
+};
+
+const toggleDropdown = function () {
+    toggleVisibility("dropdown-list");
+};
+
+const toggleVisibility = function (id) {
+    const DISPLAY_BLOCK = "block";
+    const DISPLAY_NONE = "none";
+  
+    const $nav_collapse = document.getElementById(id);
+    let display = $nav_collapse.style.display;
+    if (!display || display === DISPLAY_NONE) {
+        $nav_collapse.style.display = DISPLAY_BLOCK;
+    } else {
+      $nav_collapse.style.display = DISPLAY_NONE;
+    }
+};
+  
