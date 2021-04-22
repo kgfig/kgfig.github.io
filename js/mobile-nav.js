9fe3f10d1b380083412e421e2559d6f115f7303b
KG Figueroa <kathleengay.figueroa@gmail.com>
2021-04-22 15:35:51 +0800

    Slide toggle nav on mobile

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -11,11 +11,12 @@ const toggleVisibility = function (id) {
     const DISPLAY_NONE = "none";
   
     const $nav_collapse = document.getElementById(id);
-    let display = $nav_collapse.style.display;
-    if (!display || display === DISPLAY_NONE) {
-        $nav_collapse.style.display = DISPLAY_BLOCK;
-    } else {
-      $nav_collapse.style.display = DISPLAY_NONE;
-    }
+    // let display = $nav_collapse.style.display;
+    // if (!display || display === DISPLAY_NONE) {
+    //     $nav_collapse.style.display = DISPLAY_BLOCK;
+    // } else {
+    //   $nav_collapse.style.display = DISPLAY_NONE;
+    // }
+    slideToggle($nav_collapse, 150);
 };
   
